0e2d37d2e98efef30278a3f639bdd793368e7cc2
Dale Harris <112442571+syntheny@users.noreply.github.com>
2023-07-13 16:35:30 -0600

    Update hw.sh

--- hw.sh
+++ hw.sh
@@ -1,4 +1,19 @@
-#!/usr/bin/bash
+#!/bin/bash
 echo "hello world"
 
 echo "goodbye cruel world"
+
+N=${1:-10}
+
+a=0
+b=1
+
+echo "The Fibonacci series is : "
+
+for (( i=0; i<N; i++ ))
+do
+    echo -e "$i\t$a"
+    fn=$((a + b))
+    a=$b
+    b=$fn
+done
